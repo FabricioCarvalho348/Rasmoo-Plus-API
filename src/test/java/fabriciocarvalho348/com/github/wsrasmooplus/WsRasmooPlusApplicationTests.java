901efb1d0cb871624927ff7f8a34a7a810b8c842
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-02-27 14:17:45 -0300

    feat: Iniciando projeto utilizando Java 17, Spring Boot, Spring Security, Spring HATEOAS, Spring JPA, Flyway, MySQL, JUnit5, Mockito, Docker, Redis.

---
+++ src/test/java/fabriciocarvalho348/com/github/wsrasmooplus/WsRasmooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package fabriciocarvalho348.com.github.wsrasmooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRasmooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
