ea1611dfda885008a505ae2eb3dc5269d744986e
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-03-03 10:14:21 -0300

    feat: Adicionando o lombok as nossas entidades.

--- src/main/java/fabriciocarvalho348/com/github/wsrasmooplus/model/UserType.java
+++ src/main/java/fabriciocarvalho348/com/github/wsrasmooplus/model/UserType.java
@@ -1,9 +1,17 @@
 package fabriciocarvalho348.com.github.wsrasmooplus.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "user_type")
 public class UserType implements Serializable {
@@ -17,37 +25,5 @@ public class UserType implements Serializable {
 
     private String description;
 
-    public UserType() {
-    }
-
-    public UserType(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 }
