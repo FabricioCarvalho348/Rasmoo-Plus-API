172ef172dd558ba4beaa88915f510b06df1e3815
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-03-12 12:41:58 -0300

    feat: implementando o Spring HATEOAS na entidade SubscriptionType e no método findById na camada Service.

--- src/main/java/fabriciocarvalho348/com/github/wsrasmooplus/service/impl/SubscriptionTypeServiceImpl.java
+++ src/main/java/fabriciocarvalho348/com/github/wsrasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -1,5 +1,6 @@
 package fabriciocarvalho348.com.github.wsrasmooplus.service.impl;
 
+import fabriciocarvalho348.com.github.wsrasmooplus.controller.SubscriptionTypeController;
 import fabriciocarvalho348.com.github.wsrasmooplus.dto.SubscriptionTypeDto;
 import fabriciocarvalho348.com.github.wsrasmooplus.exception.BadRequestException;
 import fabriciocarvalho348.com.github.wsrasmooplus.exception.NotFoundException;
@@ -7,6 +8,7 @@ import fabriciocarvalho348.com.github.wsrasmooplus.mapper.SubscriptionTypeMapper
 import fabriciocarvalho348.com.github.wsrasmooplus.model.SubscriptionType;
 import fabriciocarvalho348.com.github.wsrasmooplus.repository.SubscriptionTypeRepository;
 import fabriciocarvalho348.com.github.wsrasmooplus.service.SubscriptionTypeService;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,8 @@ import java.util.Optional;
 @Service
 public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
+    private static final String UPDATE = "update";
+    private static final String DELETE = "delete";
     private final SubscriptionTypeRepository subscriptionTypeRepository;
 
     SubscriptionTypeServiceImpl(SubscriptionTypeRepository subscriptionTypeRepository) {
@@ -29,7 +33,16 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
     @Override
     public SubscriptionType findById(Long id) {
-        return getSubscriptionType(id);
+        return getSubscriptionType(id).add(WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(SubscriptionTypeController.class).findById(id))
+                .withSelfRel()
+        ).add(WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(SubscriptionTypeController.class).update(id, new SubscriptionTypeDto()))
+                .withRel(UPDATE)
+        ).add(WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(SubscriptionTypeController.class).delete(id))
+                .withRel(DELETE)
+        );
     }
 
     @Override
